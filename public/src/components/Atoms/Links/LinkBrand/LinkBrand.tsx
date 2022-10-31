5f101e033414bf35a75a5ad86f3c62515f117447
john_dev <johnhaimez314@gmail.com>
2022-10-31 07:55:45 +0100

    chore : add ref for close menu mobile

--- public/src/components/Atoms/Links/LinkBrand/LinkBrand.tsx
+++ public/src/components/Atoms/Links/LinkBrand/LinkBrand.tsx
@@ -1,10 +1,12 @@
 import { A } from '@solidjs/router'
 import { Component } from 'solid-js'
+import { refMenus } from '../../../../store/app/app.store'
 import './LinkBrand.css'
 
 export const LinkBrand: Component = () => {
   return (
     <A
+      ref={refMenus}
       class='link-brand-principal'
       activeClass='link-brand-active'
       inactiveClass='link-brand-inactive'
--- public/src/templates/TemplatePublic/TemplatePublic.tsx
+++ public/src/templates/TemplatePublic/TemplatePublic.tsx
@@ -1,13 +1,14 @@
-import { Component } from 'solid-js'
+import { Component, onMount } from 'solid-js'
 import { Outlet } from '@solidjs/router'
 import { Header } from '../../components/Organisms/Header/Header'
 import './TemplatePublic.css'
+import { refMenus } from '../../store/app/app.store'
 
 export const TemplatePublic: Component = () => {
   return (
     <>
       <Header />
-      <main class='template-public'>
+      <main class='template-public' ref={refMenus}>
         <Outlet />
       </main>
     </>
