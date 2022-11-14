eb39da0ecc7e964287c40e4423d473e4dc19d750
john_dev <johnhaimez314@gmail.com>
2022-11-14 19:09:08 +0100

    chore : struncture folder + create icons

---
+++ frontend/src/components/Atoms/Icons/AddIcon.tsx
@@ -0,0 +1,26 @@
+import { Ticon } from '../../../types/IconType'
+import React from 'react'
+
+/**
+ * AddIcon
+ * @param size? string | undefined;
+ * @param color? string | null | undefined;
+ * @returns
+ */
+export function AddIcon({ color, size }: Ticon) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth='1.5'
+      stroke='currentColor'
+      className={`${color} ${size ?? 'w-6 h-6'}`}>
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
+      />
+    </svg>
+  )
+}
---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,6 @@
+import { useState } from 'react'
+import './App.css'
+
+export function App() {
+  return <div>coucou</div>
+}
