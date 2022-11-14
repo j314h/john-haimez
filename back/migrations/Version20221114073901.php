14f9c23376bb02b01e8873813834329d4836632b
john_dev <johnhaimez314@gmail.com>
2022-11-14 08:50:29 +0100

    chore : delete version migration, reset all

--- back/migrations/Version20221114073901.php
+++ back/migrations/Version20221114073901.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221106074836 extends AbstractMigration
+final class Version20221114073901 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,17 +20,12 @@ final class Version20221106074836 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE media ADD competence_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE media ADD CONSTRAINT FK_6A2CA10C15761DAB FOREIGN KEY (competence_id) REFERENCES competence (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('CREATE INDEX IDX_6A2CA10C15761DAB ON media (competence_id)');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE media DROP CONSTRAINT FK_6A2CA10C15761DAB');
-        $this->addSql('DROP INDEX IDX_6A2CA10C15761DAB');
-        $this->addSql('ALTER TABLE media DROP competence_id');
     }
 }
