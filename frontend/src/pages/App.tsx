02baed39355efcdded5a2591a62f975039a53223
john_dev <johnhaimez314@gmail.com>
2022-11-14 22:18:51 +0100

    chore : install react router and add structur of navigation

--- frontend/src/pages/App.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react'
-import './App.css'
-
-export function App() {
-  return <div>coucou</div>
-}
---
+++ frontend/src/templates/TemplatePublic/TemplatePublic.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export function TemplatePublic() {
+  return (
+    <>
+      <header>header</header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>footer</footer>
+    </>
+  )
+}
