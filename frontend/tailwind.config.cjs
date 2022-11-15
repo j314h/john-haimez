1dd63089b7ad5523be95c5daefe281f5664ba7e8
john_dev <johnhaimez314@gmail.com>
2022-11-15 10:25:46 +0100

    chore : add new font

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -4,6 +4,10 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        body: ['Roboto', 'sans-serif'],
+        styling: ['Rubik Distressed', 'cursive'],
+      },
       colors: {
         prim: '#F5F5F5',
         'prim-d': '#141414',
