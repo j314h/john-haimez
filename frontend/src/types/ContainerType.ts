80ce9231dcb9d94d58dae926d8478e8b6d515f89
john_dev <johnhaimez314@gmail.com>
2022-11-16 11:22:11 +0100

    chore : create container

---
+++ frontend/src/types/ContainerType.ts
@@ -0,0 +1,19 @@
+/**
+ * enum for type container primary
+ */
+export enum EcontainerPrimary {
+  SMALLXL = 'small-xl',
+  SMALL = 'small',
+  MEDIUMXL = 'medium-xl',
+  MEDIUM = 'medium',
+  LARGEXL = 'large-xl',
+  LARGE = 'large',
+}
+
+/**
+ * type for container primary
+ */
+export type TcontainerPrimary = {
+  children: JSX.Element | string | null | JSX.Element[]
+  typeContainer: EcontainerPrimary
+}
