7aefa918cfec88a27904f418fab589d8b25d1a9b
john_dev <johnhaimez314@gmail.com>
2022-11-19 10:41:29 +0100

    chore : update button dark mode

--- frontend/src/_components/Molecules/Buttons/ButtonModeDark/ButtonModeDark.tsx
+++ frontend/src/_components/Molecules/Buttons/ButtonModeDark/ButtonModeDark.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { TbuttonModeDark } from '../../../../types'
 import { ThemeStore } from '../../../../_store'
 import { ButtonIcon } from '../../../Atoms/Buttons/ButtonIcon/ButtonIcon'
 import { MoonIcon } from '../../../Atoms/Icons/MoonIcon'
 import { SunIcon } from '../../../Atoms/Icons/SunIcon'
 
-export function ButtonModeDark() {
+export function ButtonModeDark({ addClass }: TbuttonModeDark) {
   const modeDark = ThemeStore.useModeDark()
   return (
-    <ButtonIcon click={() => ThemeStore.switchModeDark(modeDark)}>
+    <ButtonIcon
+      addClass={addClass}
+      click={() => ThemeStore.switchModeDark(modeDark)}>
       {modeDark ? (
         <SunIcon color='text-warning-d' />
       ) : (
