02c115303b09364b9fbd7b0c45eacfc4f7c42983
john_dev <johnhaimez314@gmail.com>
2022-11-21 06:13:59 +0100

    chore : starting design footer

--- frontend/src/_components/Organisms/Footer/Footer.tsx
+++ frontend/src/_components/Organisms/Footer/Footer.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { LinkBrand, MenuSecondary } from '../..'
+import './Footer.css'
 
 export function Footer() {
   return (
-    <footer className='bg-sec dark:bg-sec-d text-black dark:text-white'>
-      Footer
+    <footer className='footer'>
+      <div className='flex flex-col-reverse items-center'>
+        {/* link home */}
+        <div className='flex items-center flex-col'>
+          <LinkBrand>John Haimez</LinkBrand>
+          <p className='text-s2 sm:text-s3 font-medium'>
+            Développeur Web Javascript et Php
+          </p>
+        </div>
+
+        {/* menu secondary */}
+        <MenuSecondary addClass='mb-8' />
+      </div>
+      {/* copyright */}
+      <div>
+        <p></p>
+      </div>
     </footer>
   )
 }
