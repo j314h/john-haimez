03e40ac69c5a17742279ba598a0f6aea0f05b6b2
john_dev <johnhaimez314@gmail.com>
2022-11-21 06:13:46 +0100

    chore : add comment header

--- frontend/src/_components/Organisms/Header/Header.tsx
+++ frontend/src/_components/Organisms/Header/Header.tsx
@@ -5,11 +5,13 @@ import './Header.css'
 export function Header() {
   return (
     <header className='header'>
+      {/* link home + btn dark mode */}
       <div>
-        <LinkBrand link='/'>JH</LinkBrand>
+        <LinkBrand>JH</LinkBrand>
         <ButtonModeDark addClass='ml-2 sm:ml-6' />
       </div>
 
+      {/* btn menu moble + menu primary */}
       <div>
         <MenuPrimary />
         <ButtonMenuMobile />
