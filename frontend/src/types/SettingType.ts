ea2e975192097c7e9649508412d2d0b016b46a96
john_dev <johnhaimez314@gmail.com>
2022-11-22 08:54:54 +0100

    chore : add setting model type

--- frontend/src/types/SettingType.ts
+++ frontend/src/types/SettingType.ts
@@ -2,6 +2,16 @@
  * interface for model setting
  */
 export interface Isetting {
+  '@id'?: string
+  '@type'?: string
   id?: number
   copyright: string
+}
+
+export interface IsettingsResponseAxios {
+  '@context': string
+  '@id': string
+  '@type': string
+  'hydra:member': Isetting[]
+  'hydra:totalItems': number
 }
