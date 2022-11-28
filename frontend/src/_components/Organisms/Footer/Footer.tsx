95cb87a1220ad41ae73a3ce5a7c2a197371d31a5
john_dev <johnhaimez314@gmail.com>
2022-11-28 09:39:55 +0100

    chore : change design footer and profile display

--- frontend/src/_components/Organisms/Footer/Footer.tsx
+++ frontend/src/_components/Organisms/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { LinkBrand, MenuSecondary, SpinnerCircle } from '../..'
 import { SettingService } from '../../../_store'
 import './Footer.css'
--- frontend/src/_components/Organisms/ProfileDisplayPublic/ProfileDisplayPublic.tsx
+++ frontend/src/_components/Organisms/ProfileDisplayPublic/ProfileDisplayPublic.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { EtitlePrimary, TprofileDisplayPublic } from '../../../types'
 import { ProfileService } from '../../../_store'
 import { LinkText, SpinnerCircle, TitlePrimary } from '../../'
